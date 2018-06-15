2eff9119c492e9611229c8abad84fd50943fbcba
Alex Kang <thkang0@gmail.com>
2018-06-15 16:25:53 +0900

    modified predict.py

--- predict.py
+++ predict.py
@@ -7,12 +7,11 @@ import sys
 from google.oauth2 import service_account
 
 
-project = "tribal-mapper-199302"
-model = "thkang0_test"
-version = "thkang0_test_201806071740_base"
+project = "<GCP_project_id>"
+model = "<GCP_ML_ENGINE_MODEL>"
+version = "<GCP_ML_ENGINE_MODEL_VERSION>"
 
-credentials = service_account.Credentials.from_service_account_file('/home/thkang0/gcp-service-account.json')
-#credentials = GoogleCredentials.get_application_default()
+credentials = service_account.Credentials.from_service_account_file('~/gcp-service-account.json')
 ml_service = discovery.build('ml', 'v1', credentials=credentials)
 
 def get_prediction(instance, project, model, version):
@@ -26,9 +25,6 @@ def get_prediction(instance, project, model, version):
 if __name__ == '__main__':
 
   file_path = sys.argv[1]
-  #project = sys.argv[2]
-  #model = sys.argv[3]
-  #version = sys.argv[4] if len(sys.argv) > 4 else ''
 
   with open(file_path) as ff:
     content = ff.read()
@@ -37,9 +33,7 @@ if __name__ == '__main__':
 
   result = get_prediction(instance, project,  model, version)
 
-  #json_data = json.loads(result['predictions'][0])
   json_dump_data = json.dumps(result['predictions'])
-  #print result['predictions']
 
   json_data = json.loads(json_dump_data)
   son = json_data[0]["scores"][1]
